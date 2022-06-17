030396516988e8088d0cf4e4317a85d3a6bbe10b
yuanzishuai <249175311@qq.com>
2022-06-17 21:24:45 +0800

    change eglot to lsp

--- init.el
+++ init.el
@@ -4,9 +4,14 @@
 (when (>= emacs-major-version 24)
   (require 'package)
   (setq package-archives
-        '(("melpa-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-          ("org-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
-          ("gnu-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/"))))
+        '(
+          ;; ("melpa-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+          ;;           ("org-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+          ;;           ("gnu-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+          ("melpa-cn" . "http://elpa.zilongshanren.com/melpa/")
+          ("org-cn"   . "http://elpa.zilongshanren.com/org/")
+          ("gnu-cn"   . "http://elpa.zilongshanren.com/gnu/")
+          )))
 
 (package-initialize)
 ;;防止反复调用 package-refresh-contents 会影响加载速度
@@ -19,10 +24,10 @@
 ;; install packages
 (defun install-my-packages (my-packages)
   (let ((my-package (car my-packages)))
-     (message "%s" my-package)
-     (unless (package-installed-p my-package)
-       (package-install my-package)
-     (install-my-packages (cdr my-packages)))))
+    (message "%s" my-package)
+    (unless (package-installed-p my-package)
+      (package-install my-package)
+      (install-my-packages (cdr my-packages)))))
 
 (install-my-packages my-packages)
 
@@ -47,17 +52,18 @@
   (require 'use-package)
   (setq use-package-always-ensure t)
   (setq use-package-always-defer t)
-)
+  )
 
 (use-package evil
   :init (progn
           (setq evil-want-C-i-jump nil)
+          (setq evil-undo-system 'undo-redo)
           (evil-mode)
           (cond 
            ((string-equal system-type "windows-nt") (add-hook 'evil-normal-state-entry-hook '(lambda () (w32-set-ime-open-status nil))))))
   :hook
   (org-mode . (lambda () (setq evil-shift-width 2)))
-)
+  )
 
 
 
@@ -78,7 +84,9 @@
    '("57e3f215bef8784157991c4957965aa31bac935aca011b29d7d8e113a652b693" default))
  '(minibuffer-prompt-properties '(read-only t cursor-intangible t face minibuffer-prompt))
  '(org-agenda-files '("d:/NutStore/note/GTD/read.org"))
- '(package-selected-packages '(eglot embark-consult embark marginalia Buffer-menu evil)))
+ '(package-selected-packages
+   '(spinner gnu-elpa-keyring-update eglot embark-consult embark marginalia Buffer-menu evil))
+ '(warning-suppress-types '((use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/emacs-code-config.el
+++ lisp/emacs-code-config.el
@@ -1,34 +1,44 @@
 (use-package company
   :hook
   (after-init . global-company-mode)
-  )
+  :bind
+  (:map company-mode-map
+        ("<TAB>" . #'completion-at-point)))
 
 (use-package yasnippet-snippets
   :bind ("C-c i" . #'yas-insert-snippet)
-  :hook 
-  (flymake-mode . yas-global-mode)
-  )
+)
 
 (use-package emmet-mode
-  :hook
-  (html-mode . emmet-mode)
-  (css-mode . emmet-mode)
   :bind (:map emmet-mode-keymap
          ("M-/" . emmet-expand-yas)))
 
+;; (use-package eglot
+;;   :hook
+;;   (web-mode . eglot-ensure)
+;;   :bind
+;;   (:map eglot-mode-map
+;;         ("<tab>" . #'company-complete)
+;;         ("C-c e n" . #'flymake-goto-next-error)
+;;         ("C-c e p" . #'flymake-goto-prev-error)
+;;         ("C-c e f" . #'eglot-format)
+;;         ("C-c e r" . #'eglot-rename)))
 
-(use-package eglot
-  :hook
-  (js-mode . eglot-ensure)
-  (html-mode . eglot-ensure)
-  (css-mode . eglot-ensure)
-  :bind
-  (:map eglot-mode-map
-        ("C-c <tab>" . #'company-complete)
-        ("C-c e n" . #'flymake-goto-next-error)
-        ("C-c e p" . #'flymake-goto-prev-error)
-        ("C-c e f" . #'eglot-format)
-        ("C-c e r" . #'eglot-rename))
-  )
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (web-mode . lsp)
+         (web-mode . emmet-mode)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+;; optionally
+
+(use-package web-mode
+  :init
+  (add-to-list 'auto-mode-alist 
+               '("\\.html?\\'" . web-mode)))
 
 (provide 'emacs-code-config)
