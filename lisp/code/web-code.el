e711dd64edd3a9ec033b13f9beba2fdf15a51258
yuanzishuai <yuanzishuai@qq.com>
2022-09-15 21:54:18 +0800

    custom org export to html setting

--- lisp/code/web-code.el
+++ lisp/code/web-code.el
@@ -41,22 +41,12 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
 (straight-use-package
  '(lsp-volar :type git :host github :repo "jadestrong/lsp-volar"))
 (use-package lsp-volar
-  :straight t)
+  :defer nil
+  :straight t
+  :init
+  (setq lsp-typescript-suggest-auto-imports nil))
 ;;vue config end
 (provide 'web-code)
--- lisp/emacs-org-config.el
+++ lisp/emacs-org-config.el
@@ -12,7 +12,15 @@
           org-log-done t
           org-log-into-drawer t
           org-ellipsis " ▼"
-          org-hide-emphasis-markers t)
+          org-hide-emphasis-markers t
+		  ;; org-html-head-include-default-style nil
+		  org-html-head-extra "<style>
+                                    pre.src{background:#343131;color:white;}
+                                    #content{max-width:1800px;}
+                                    p{max-width:800px;}
+                                    li{max-width:800px;}
+                              </style>"
+		  )
     ;;修改无序列表的符号 - 
     (font-lock-add-keywords 'org-mode 
                             '(("^ *\\([-]\\) "
@@ -21,6 +29,7 @@
    'org-babel-load-languages
    '((js . t)
      (shell . t)))
+
   :hook 
   (org-mode . org-indent-mode)
   ;; (org-mode . org-num-mode)
@@ -35,5 +44,40 @@
   (org-mode . org-bullets-mode)
   )
 
+(use-package htmlize)
+
+
+;; begin 
+;; add "#+setupfile:" to every .org buffer
+(defcustom yzs/setup-file '("https://fniessen.github.io/org-html-themes/org/theme-readtheorg.setup")
+  "list of string with setup file or functions.
+
+Functions must take care of keyword themselves and must return
+strings.")
+
+(defcustom yzs/setup-file-keyword "#+setupfile: "
+  "Keyword used by `yzs/org-export-setup-files'")
+
+(defun yzs/org-export-setup-files (backend)
+  "Include setup-files automatically.  
+
+See also `yzs/setup-file' and `yzs/setup-file-keyword'"
+  ;; TODO: Resolve local files.
+  ;; TODO: Better control over where contents is inserted.
+  (save-excursion
+	(goto-char (point-min))
+	(insert (mapconcat
+			 (lambda (elm)
+			   (cond ((stringp elm)
+					  (concat yzs/setup-file-keyword elm))
+					 (functionp elm) (funcall elm)
+					 (t (error "`yzs/setup-file' should contain only strings and functions!"))))
+			 yzs/setup-file
+			 "\n") "\n")))
+
+(add-hook 'org-export-before-processing-hook
+      'yzs/org-export-setup-files)
+
+;; end
 
 (provide 'emacs-org-config)
