a858e033ffacddde78402cf7b7685332867b97e4
yuanzishuai <yuanzishuai@qq.com>
2022-11-03 01:44:58 +0800

    优化key-binding的代码

--- lisp/emacs-key-binding-config.el
+++ lisp/emacs-key-binding-config.el
@@ -1,31 +1,6 @@
 (use-package general)
 
 (general-define-key
- :prefix "C-c"
- :states '(normal insert)
- :keymaps 'yas-minor-mode-map
- "y" '(:which-key "yasnippet")
- "y i" 'yas-insert-snippet
- "y t" 'yas-describe-tables
- )
- 
-(general-define-key
- :prefix "C-c"
- :states '(normal insert)
- :keymaps 'global-map
- "c" '(:which-key "Custom")
- "o" '(:which-key "Org")
- "!" '(:which-key "check")
- "g" '(:which-key "Git")
- )
-
-(general-define-key
- :prefix "g"
- :states '(normal motion)
- "c c" 'comment-line
- )
-
-(general-define-key
  :states '(normal insert motion emacs)
  "C-M-<left>" 'evil-window-decrease-width
  "C-M-<right>" 'evil-window-increase-width
@@ -33,39 +8,47 @@
  "C-M-<down>" 'evil-window-decrease-height
  )
 
-;;Org
 (general-define-key
+ :prefix "C-c"
  :states '(normal insert motion emacs)
- :prefix "C-c o"
- "f" 'org-roam-node-find
- "i" 'org-roam-node-insert
- "t" 'org-roam-buffer-toggle
- "c" 'org-id-get-create
- "g" 'org-roam-ui-mode
- "s" 'org-roam-db-sync
- "r" 'org-redisplay-inline-images
+ :keymaps 'global-map
+ "c" '(:which-key "Custom")
+ "c b" 'yzs/open-file-in-browser
+ "c d" 'yzs/open-directory
+ "c c" 'yzs/run-code
+ "c s" 'yzs/open-file-in-live-server
+ "c o" 'yzs/open-file-in-system
+ "c o" '(:which-key "Org")
+ "o f" 'org-roam-node-find
+ "o i" 'org-roam-node-insert
+ "o t" 'org-roam-buffer-toggle
+ "o c" 'org-id-get-create
+ "o g" 'org-roam-ui-mode
+ "o s" 'org-roam-db-sync
+ "o r" 'org-redisplay-inline-images
+ "!" '(:which-key "Check")
+ "g" '(:which-key "Git")
+ "g n" 'git-gutter:next-hunk
+ "g p" 'git-gutter:previous-hunk
+ "g r" 'git-gutter:revert-hunk
+ "g t" 'git-gutter:toggle
+ "g v" 'git-gutter:popup-hunk
  )
 
-;; Custom
 (general-define-key
- :states '(normal insert motion emacs)
- :prefix "C-c c"
- "b" 'yzs/open-file-in-browser
- "d" 'yzs/open-directory
- "c" 'yzs/run-code
- "s" 'yzs/open-file-in-live-server
- "o" 'yzs/open-file-in-system
+ :prefix "C-c"
+ :states '(normal insert)
+ :keymaps 'yas-minor-mode-map
+ "y" '(:which-key "yasnippet")
+ "y i" 'yas-insert-snippet
+ "y t" 'yas-describe-tables
  )
 
-;; Git
 (general-define-key
- :states '(normal insert motion emacs)
- :prefix "C-c g"
- "n" 'git-gutter:next-hunk
- "p" 'git-gutter:previous-hunk
- "r" 'git-gutter:revert-hunk
- "t" 'git-gutter:toggle
- "v" 'git-gutter:popup-hunk
+ :prefix "g"
+ :states '(motion normal)
+ "." 'repeat
+ "c c" 'comment-line
  )
 
 (provide 'emacs-key-binding-config)
