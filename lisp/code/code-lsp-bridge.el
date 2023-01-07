e4751d2594d9940fcf8a2030d6703ca25105c2bb
yuanzishuai <yuanzishuai@qq.com>
2023-01-07 20:40:25 +0800

    优化 lsp-bridge 配置

--- lisp/code/code-lsp-bridge.el
+++ lisp/code/code-lsp-bridge.el
@@ -1,4 +1,4 @@
-(use-package lsp-bridge 
+(use-package lsp-bridge
   :ensure nil
   :after yasnippet
   :load-path "~/.emacs.d/straight/repos/lsp-bridge/acm"
@@ -6,6 +6,10 @@
   :straight '(lsp-bridge :type git :host github :repo "manateelazycat/lsp-bridge")
   :init
   (global-lsp-bridge-mode)
-  )
+  :bind
+  (:map acm-mode-map
+		("RET" . nil))
+  :custom
+  (lsp-bridge-enable-hover-diagnostic t))
 
 (provide 'code-lsp-bridge)
