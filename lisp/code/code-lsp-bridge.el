8e4b9d760073b879aef9b3d2a8b968954899dc73
yuanzishuai <yuanzishuai@qq.com>
2023-02-19 21:06:37 +0800

    add submodule lsp-bridge

--- lisp/code/code-lsp-bridge.el
+++ lisp/code/code-lsp-bridge.el
@@ -2,35 +2,11 @@
   :if (display-graphic-p)
   :ensure nil
   :after yasnippet
-  :load-path "~/.emacs.d/straight/repos/lsp-bridge/acm"
-  :load-path "~/.emacs.d/straight/repos/lsp-bridge"
-  :straight '(lsp-bridge :type git :host github :repo "manateelazycat/lsp-bridge")
+  :load-path "~/.emacs.d/site-lisp/lsp-bridge"
   :init
+  (require 'lsp-bridge)
   (global-lsp-bridge-mode)
-  :bind
-  (:map acm-mode-map
-		("RET" . nil))
   :config
-  ;; (defun lsp-bridge-not-in-string ()
-  ;; 	(interactive)
-  ;; 	"Hide completion if cursor in string area."
-  ;; 	(or
-  ;; 	 (memq major-mode '(typescript-mode javascript-mode js-mode js2-mode web-mode))
-  ;; 	 ;; Allow sdcv completion in string area
-  ;; 	 acm-enable-search-sdcv-words
-  ;; 	 ;; Allow volar popup completion menu in string.
-  ;; 	 (and (boundp 'acm-backend-lsp-filepath)
-  ;; 		  acm-backend-lsp-filepath
-  ;; 		  (string-suffix-p ".vue" acm-backend-lsp-filepath))
-  ;; 	 ;; Other language not allowed popup completion in string, it's annoy
-  ;; 	 (not (acm-in-string-p))
-  ;; 	 ;; Allow popup completion menu for string interpolation
-  ;; 	 (lsp-bridge-string-interpolation-p lsp-bridge-string-interpolation-open-chars-alist)
-  ;; 	 ;; Allow file path completion in string area
-  ;; 	 (ignore-errors
-  ;; 	   (and (thing-at-point 'filename)
-  ;; 			(file-exists-p (file-name-directory (thing-at-point 'filename))))))
-  ;; 	)
   (add-to-list 'lsp-bridge-single-lang-server-mode-list '(json-mode . "vscode-json-language-server"))
 
   (when (assoc 'typescript-mode lsp-bridge-string-interpolation-open-chars-alist)
@@ -47,6 +23,9 @@
   (:map lsp-bridge-mode-map
 		("M-/" . 'lsp-bridge-popup-complete-menu)
 		([remap evil-goto-definition] . 'lsp-bridge-find-def))
-  )
+  
+  (:map acm-mode-map
+		("RET" . nil))
+)
 
 (provide 'code-lsp-bridge)
