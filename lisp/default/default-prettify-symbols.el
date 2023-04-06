363d7bac171704bef6325565b3841459a196496e
yuanzishuai <yuanzishuai@qq.com>
2023-04-06 19:31:28 +0800

    change olivetty hook

--- lisp/default/default-prettify-symbols.el
+++ lisp/default/default-prettify-symbols.el
@@ -48,6 +48,7 @@
 (when (display-graphic-p)
   (yzs/add-prettify-symbols-to-mode 'org-mode yzs/symbols-org-mode)
   (yzs/add-prettify-symbols-to-mode 'emacs-lisp-mode nil)
+  (yzs/add-prettify-symbols-to-mode 'prog-mode)
   ;; (yzs/add-prettify-symbols-to-mode 'corfu-mode nil)
   )
 
