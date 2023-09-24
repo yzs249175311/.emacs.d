77106df03ed39a2e78c5ab7ac2dface8876d07f6
yuanzishuai <yuanzishuai@qq.com>
2023-09-24 21:11:19 +0800

    add html prettier

--- lisp/emacs-ui-config.el
+++ lisp/emacs-ui-config.el
@@ -173,7 +173,8 @@
 ;;-------------------------custom-----------------------------------
 (defun yzs/emacs-ui-config-theme ()
   "主题配置"
-  (load-theme 'doom-tomorrow-night t)
+  (load-theme 'doom-material-dark t)
+
   (custom-set-faces
    ;; '(org-block ((t (:background "gray9"))))
    ;; '(line-number-current-line ((t (:foreground "gold"))))
