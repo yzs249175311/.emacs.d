e1dbcfe526e8c034de75e15506b58add751b98da
yuanzishuai <yuanzishuai@qq.com>
2023-12-18 12:31:50 +0800

    add bun run

--- lisp/custom/custom-run-code-command.el
+++ lisp/custom/custom-run-code-command.el
@@ -67,6 +67,8 @@ OPTION 表示命令的选项."
 (yzs/generate-run-code-command-macro "ts" yzs/run--ts-esm "ts-node-esm" (filename))
 (yzs/generate-run-code-command-macro "ts" yzs/run--ts-tsx "tsx" (filename))
 (yzs/generate-run-code-command-macro "ts" yzs/run--ts-tsx-watch "tsx" ("watch " filename))
+(yzs/generate-run-code-command-macro "ts" yzs/run--ts-bun "bun" (filename))
+(yzs/generate-run-code-command-macro "ts" yzs/run--ts-bun-watch "bun" ("--watch " filename))
 (yzs/generate-run-code-command-macro "py" yzs/run--py "python" (filename))
 
 (provide 'custom-run-code-command)
