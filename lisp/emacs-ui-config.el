88b261be8cccbf4d36666825726b1f211e989ee1
yuanzishuai <yuanzishuai@qq.com>
2023-12-19 21:48:34 +0800

    change theme

--- lisp/emacs-ui-config.el
+++ lisp/emacs-ui-config.el
@@ -174,7 +174,7 @@
 ;;-------------------------custom-----------------------------------
 (defun yzs/emacs-ui-config-theme ()
   "主题配置"
-  (load-theme 'doom-oceanic-next t)
+  (load-theme 'doom-one t)
 
   (custom-set-faces
    ;; '(org-block ((t (:background "gray9"))))
