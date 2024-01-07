ff950171979a816be8085e5f3723f19c15541b9b
yuanzishuai <yuanzishuai@qq.com>
2024-01-07 14:41:19 +0800

    调整web

--- lisp/code/code-web.el
+++ lisp/code/code-web.el
@@ -10,6 +10,7 @@
   (web-mode . emmet-mode)
   (scss-mode . emmet-mode)
   (css-mode . emmet-mode)
+  (css-ts-mode . emmet-mode)
   (tsx-ts-mode . emmet-mode)
   (js-ts-mode . emmet-mode)
   ;; (emmet-mode . emmet-preview-mode)
@@ -18,7 +19,7 @@
   )
 
 (use-package web-mode
-  :mode ("\\.html?\\'" "\\.vue\\'" "\\.jsx" "\\.tsx")
+  :mode ("\\.html?\\'" "\\.vue\\'" "\\.jsx\\'" "\\.tsx\\'")
   :config
   (add-to-list 'hs-special-modes-alist '(web-mode  "{\\|<[^/>]+?" "}\\|</[^/>]*[^/]>" "<!--" web-mode-forward-sexp nil))
   ;; (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
